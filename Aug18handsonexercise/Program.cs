0dd73dbbb6523dd6ebf2977626583c018e676182
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-19 14:33:16 +0530

    char

--- Aug18handsonexercise/Program.cs
+++ Aug18handsonexercise/Program.cs
@@ -5,21 +5,20 @@
         static void Main(string[] args)
         {
             int i, j;
-            Console.WriteLine("Enter the numbers");
-            i=Convert.ToInt32(Console.ReadLine());
-            j=Convert.ToInt32(Console.ReadLine());
-            if (i > j)
+            Console.WriteLine("Enter the characters");
+            i=Convert.ToChar(Console.ReadLine());
+            j=Convert.ToChar(Console.ReadLine());
+            if (i == j)
             {
-                Console.WriteLine("{0} is max", i);
-                Console.WriteLine("{0} is min", j);
+                Console.WriteLine("Characters are same");
+               
             }
-            else if (i == j)
-                Console.WriteLine("{0} is equals {1}", i, j);
             else
             {
-                Console.WriteLine("{0} is max", j);
-                Console.WriteLine("{0} is min", i);
+                Console.WriteLine("Characters are not same");
+                
             }
+            Console.ReadLine();
         }
     }
 }
