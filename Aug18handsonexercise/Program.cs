f026d250d2cbe30612cbaff1d394d303ca20fc76
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-19 17:33:15 +0530

    committed

--- Aug18handsonexercise/Program.cs
+++ Aug18handsonexercise/Program.cs
@@ -6,30 +6,47 @@
         {
             int i;
             Console.WriteLine("Enter the numbers");
-            i=Convert.ToInt32(Console.ReadLine());
-            if (i == 0)
-                Console.WriteLine("zero");
-            else if (i % 2 == 0)
-            {
-                Console.WriteLine("even");
-                if (i % 10 == 0)
-                {
-                    Console.WriteLine("multiple of 10");
-                }
-                else
-                {
-                    Console.WriteLine("not a multiple of 10");
-                }
-                if (i > 100)
-                    Console.WriteLine("too large");
-            }
-            else
-            {
-                Console.WriteLine("odd");
-                Console.WriteLine("not a multiple of 10");
-                if (i > 100)
-                    Console.WriteLine("too large");
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1.Zero\n 2.Even/odd\n 3.multiple of 10\n 4.toolarge\n");
+            int input=0;
+            while (input < 4){
+                switch (input) {
+                    case 0:
+                        if (i == 0)
+                            Console.WriteLine("zero");
+                        break;
+                    case 1:
+                        if (i % 2 == 0)
+                        {
+                            Console.WriteLine("even");
+                        }
+                        else
+                            Console.WriteLine("odd");
 
+                        break;
+                    case 2:
+                        if (i % 10 != 0)
+                        {
+                            Console.WriteLine("not a multiple of 10");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("multiple of 10");
+
+                        }
+                        break;
+                    case 3:
+                        if (i > 100)
+                            Console.WriteLine("too large");
+                        break;
+                    default:
+                        break;
+                        
+
+
+                }
+                input++;
             }
             Console.ReadLine();
         }
