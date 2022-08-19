ad735bea7a6fbfd723882bce487dfc2479c654b6
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-19 14:14:41 +0530

    max/min

--- Aug18handsonexercise/Program.cs
+++ Aug18handsonexercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int i, j;
+            Console.WriteLine("Enter the numbers");
+            i=Convert.ToInt32(Console.ReadLine());
+            j=Convert.ToInt32(Console.ReadLine());
+            if (i > j)
+            {
+                Console.WriteLine("{0} is max", i);
+                Console.WriteLine("{0} is min", j);
+            }
+            else if (i == j)
+                Console.WriteLine("{0} is equals {1}", i, j);
+            else
+            {
+                Console.WriteLine("{0} is max", j);
+                Console.WriteLine("{0} is min", i);
+            }
         }
     }
 }
