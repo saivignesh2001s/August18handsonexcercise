78027c3c8a02a2807e509cfd387d0563f3ba5e4c
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-19 15:13:02 +0530

    percent

--- Aug18handsonexercise/Program.cs
+++ Aug18handsonexercise/Program.cs
@@ -4,22 +4,29 @@
     {
         static void Main(string[] args)
         {
-            int i, j;
+            int i, j,k;
             Console.WriteLine("Enter the numbers");
             i=Convert.ToInt32(Console.ReadLine());
             j=Convert.ToInt32(Console.ReadLine());
-            if (i > j)
+            k= Convert.ToInt32(Console.ReadLine());
+            int sum;
+            float avg;
+             sum = i + j + k;
+            avg = sum / 3;
+            if (avg < 35)
             {
-                Console.WriteLine("{0} is max", i);
-                Console.WriteLine("{0} is min", j);
+                Console.WriteLine("Fail");
+                
             }
-            else if (i == j)
-                Console.WriteLine("{0} is equals {1}", i, j);
+            else if (avg>35 && avg<45)
+                Console.WriteLine("third");
+            else if (avg > 45 && avg < 60)
+                Console.WriteLine("second");
             else
             {
-                Console.WriteLine("{0} is max", j);
-                Console.WriteLine("{0} is min", i);
+                Console.WriteLine("first");
             }
+            Console.ReadLine();
         }
     }
 }
