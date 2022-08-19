b3ee9e4cd998f28ef9264cd83fa809a12bf87078
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-19 14:36:32 +0530

    string

--- Aug18handsonexercise/Program.cs
+++ Aug18handsonexercise/Program.cs
@@ -4,22 +4,14 @@
     {
         static void Main(string[] args)
         {
-            int i, j;
-            Console.WriteLine("Enter the numbers");
-            i=Convert.ToInt32(Console.ReadLine());
-            j=Convert.ToInt32(Console.ReadLine());
-            if (i > j)
-            {
-                Console.WriteLine("{0} is max", i);
-                Console.WriteLine("{0} is min", j);
-            }
-            else if (i == j)
-                Console.WriteLine("{0} is equals {1}", i, j);
-            else
-            {
-                Console.WriteLine("{0} is max", j);
-                Console.WriteLine("{0} is min", i);
-            }
+            string i, j;
+            Console.WriteLine("Enter the strings");
+            i=Console.ReadLine();
+            j=Console.ReadLine();
+            bool res;
+            res=i.Equals(j);
+            Console.WriteLine(res);
+            Console.ReadLine();
         }
     }
 }
